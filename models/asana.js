41eb587b4a9cc822976d9e8654463b105ccda850
Roksolana Odynak <roksolanaodynak1@gmail.com>
2023-02-09 13:22:16 -0800

    created asana.js model

---
+++ models/asana.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class asana extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+asana.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    nameEnglish: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nameSanskrit: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
+  },
+  {
+    hooks: {
+      beforeCreate: async (newAsanaData) => {
+        newAsanaData.password = await bcrypt.hash(newAsanaData.password, 10);
+        return newAsanaData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'asana',
+  }
+);
+
+module.exports = Asana;
