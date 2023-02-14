4d853f0bc976825fc1ef28155e4eb343e84d7458
petershaughnessy <prshaughnessy@gmail.com>
2023-02-13 19:49:03 -0800

    amended index.js seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,23 @@
 const sequelize = require('../config/config');
-const { User, Injury, Asana  } = require('../models');
+const { Focus, Asana,Asana_Focus} = require('../models');
+const seedFocus = require('./focusSeed.js')
+const asanaSeedData = require('./asanaSeed.js')
+const seedAsanaFocus =require('./asana_focusSeed.js')
+
+
 const seeddatabase = async ()=> {
-    await sequelize.sync({force:true})
+    await sequelize.sync({force:true});
+    console.log('db linked');
+
+    await seedFocus()
+    console.log("----------focus seeded-----------");
+
+    await asanaSeedData("----------asana seeded-----");
+
+
+    await seedAsanaFocus("---------asanafocus seeded------");
+
     process.exit(0)
+    
 }
 seeddatabase()
